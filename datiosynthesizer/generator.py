61a542a2091b289adce845dabeb37d33c6a2ec16
l.morales <l.morales@bbva.com>
2018-10-24 08:18:44 +0200

    Generator first files

---
+++ datiosynthesizer/generator.py
@@ -0,0 +1,10 @@
+import dask
+import dask.dataframe as df
+import pandas as pd
+import json
+import datiosynthesizer.config as config
+import datiosynthesizer.utils as utils
+import datiosynthesizer.attributes as attributes
+
+def init_random(seed: int = 0) -> None:
+    utils.set_random_seed(seed)
